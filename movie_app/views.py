6e6237c339a68ca2071ce24c3e7f080cfc72f8e3
VladimirKorolev1986 <106909929+VladimirKorolev1986@users.noreply.github.com>
2023-04-09 13:19:46 +0300

    Annotate

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
-from django.db.models import F, Sum, Max, Min, Count, Avg
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 # Create your views here.
 
 
 def show_all_movie(request):
-    movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
-    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'))
+    # movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool=Value(False),
+        str_field=Value('Hello'),
+        int_field=Value(3),
+        new_budjet=F('budget')+100
+    )
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('id'))
     return render(request, 'movie_app/all_movie.html', {
         'movies': movies,
-        'agg': agg
+        'agg': agg,
+        'count': movies.count()
     })
 
 

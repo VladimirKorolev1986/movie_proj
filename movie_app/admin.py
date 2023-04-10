5b234be8169fa9c87aa6f3d2657f96e2e3b9ffe8
VladimirKorolev1986 <106909929+VladimirKorolev1986@users.noreply.github.com>
2023-04-10 20:26:13 +0300

    rating status

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,10 +4,19 @@ from .models import Movie
 
 # Register your models here.
 
-
+@admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
     list_editable = ['rating', 'year', 'budget']
+    ordering = ['rating']
+    list_per_page = 2
 
-
-admin.site.register(Movie, MovieAdmin)
+    def rating_status(self, mov):
+        if mov.rating < 60:
+            return 'Зачем это смотреть?'
+        if mov.rating < 60:
+            return 'Разок можно глянуть'
+        if mov.rating <= 85:
+            return 'Зачет'
+        else:
+            return 'Топчик'

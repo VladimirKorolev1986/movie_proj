1c9c270a7496f822304e44c6401a0080c9ace067
VladimirKorolev1986 <106909929+VladimirKorolev1986@users.noreply.github.com>
2023-04-10 20:02:22 +0300

    edit admin panel

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Movie
-# Register your models here.
-admin.site.register(Movie)
 
+
+# Register your models here.
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+
+
+admin.site.register(Movie, MovieAdmin)
